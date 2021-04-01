8d85748db4b330f859bda5487afb8f5d3cd551d3
Lucas Henrique <lucas.henrique0023@gmail.com>
2021-04-01 13:11:19 -0300

    Implementando servico de armazenagem de fotos no disco local.
    Foi criada toda estrutura para que o arquivo possa ser armazenado
    localmente.

---
+++ src/main/java/com/algaworks/algafood/infrastructure/service/storage/StorageException.java
@@ -0,0 +1,15 @@
+package com.algaworks.algafood.infrastructure.service.storage;
+
+public class StorageException extends RuntimeException {
+
+	public StorageException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public StorageException(String message) {
+		super(message);
+	}
+
+	
+	
+}
