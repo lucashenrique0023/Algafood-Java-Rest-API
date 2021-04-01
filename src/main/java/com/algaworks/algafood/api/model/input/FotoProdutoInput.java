de558f342bf52e9e771cfff52184c899e5fbd588
Lucas Henrique <lucas.henrique0023@gmail.com>
2021-03-31 22:33:16 -0300

    Implementando Validacao ContentType do arquivo

--- src/main/java/com/algaworks/algafood/api/model/input/FotoProdutoInput.java
+++ src/main/java/com/algaworks/algafood/api/model/input/FotoProdutoInput.java
@@ -2,8 +2,10 @@ package com.algaworks.algafood.api.model.input;
 
 import javax.validation.constraints.NotBlank;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.algaworks.algafood.core.validation.FileContentType;
 import com.algaworks.algafood.core.validation.FileSize;
 import com.sun.istack.NotNull;
 
@@ -15,7 +17,8 @@ import lombok.Setter;
 public class FotoProdutoInput {
 
 	@NotNull
-	@FileSize(max = "50KB")
+	@FileSize(max = "500KB")
+	@FileContentType(allowed = { MediaType.IMAGE_JPEG_VALUE, MediaType.IMAGE_PNG_VALUE })
 	private MultipartFile arquivo;
 	
 	@NotBlank
