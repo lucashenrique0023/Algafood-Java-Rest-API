f1c95a33ef1f43e94e4e2a7d2fd58dedda488877
Lucas Henrique <lucas.henrique0023@gmail.com>
2021-04-12 21:21:35 -0300

    Configurnado DeepETag

--- src/main/java/com/algaworks/algafood/api/controllers/FormaPagamentoController.java
+++ src/main/java/com/algaworks/algafood/api/controllers/FormaPagamentoController.java
@@ -1,5 +1,6 @@
 package com.algaworks.algafood.api.controllers;
 
+import java.time.OffsetDateTime;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -18,6 +19,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.ServletWebRequest;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
 
 import com.algaworks.algafood.api.assembler.FormaPagamentoInputDisassembler;
 import com.algaworks.algafood.api.assembler.FormaPagamentoModelAssembler;
@@ -45,12 +48,26 @@ public class FormaPagamentoController {
 	
 	
 	@GetMapping
-	public ResponseEntity<List<FormaPagamentoModel>> listar(){
+	public ResponseEntity<List<FormaPagamentoModel>> listar(ServletWebRequest request) {
+		ShallowEtagHeaderFilter.disableContentCaching(request.getRequest());
+		
+		String eTag = "0";
+		
+		OffsetDateTime dataUltimaAtualizacao = formaPagamentoRepository.getDataUltimaAtualizacao();
+		
+		if (dataUltimaAtualizacao != null) {
+			eTag = String.valueOf(dataUltimaAtualizacao.toEpochSecond());
+		}
+		
+		if (request.checkNotModified(eTag)) {
+			return null;
+		}
 		
 		List<FormaPagamentoModel> formasPagamentosModel =  formaPagamentoModelAssembler.toModelList(formaPagamentoRepository.findAll());
 		
 		return ResponseEntity.ok()
-				.cacheControl(CacheControl.maxAge(10, TimeUnit.SECONDS))
+				.cacheControl(CacheControl.maxAge(10, TimeUnit.SECONDS).cachePublic())
+				.eTag(eTag)
 				.body(formasPagamentosModel);
 				
 	}
--- src/main/java/com/algaworks/algafood/domain/repository/FormaPagamentoRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/FormaPagamentoRepository.java
@@ -1,5 +1,8 @@
 package com.algaworks.algafood.domain.repository;
 
+import java.time.OffsetDateTime;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.algaworks.algafood.domain.model.FormaPagamento;
@@ -7,4 +10,7 @@ import com.algaworks.algafood.domain.model.FormaPagamento;
 @Repository
 public interface FormaPagamentoRepository extends CustomJPARepository<FormaPagamento, Long>{
 
+	@Query("select max(dataAtualizacao) from FormaPagamento")
+	OffsetDateTime getDataUltimaAtualizacao();
+	
 }
