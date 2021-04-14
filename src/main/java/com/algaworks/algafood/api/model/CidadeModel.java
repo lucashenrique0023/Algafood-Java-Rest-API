4211f193f591fc04a8ab3eb0e9268c3da8fac649
Lucas Henrique <lucas.henrique0023@gmail.com>
2021-04-14 07:36:31 -0300

    Descrevendo modelos e propriedades com @ApiModelProperty e @ApiModel

--- src/main/java/com/algaworks/algafood/api/model/CidadeModel.java
+++ src/main/java/com/algaworks/algafood/api/model/CidadeModel.java
@@ -1,13 +1,20 @@
 package com.algaworks.algafood.api.model;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
+//@ApiModel(name = "Cidade", description = "Representa uma cidade")
 @Getter
 @Setter
 public class CidadeModel {
 
+//	@ApiModelProperty(value = "ID da cidade", example = "1")
+	@ApiModelProperty(example = "1")
 	private Long id;
+	
+	@ApiModelProperty(example = "Recife")
 	private String nome;
+	
 	private EstadoModel estado;
 }
