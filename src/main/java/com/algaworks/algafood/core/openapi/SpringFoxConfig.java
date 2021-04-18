dab3ebd63d84887e3c2222722ba0d369dcbd9320
Lucas Henrique <lucas.henrique0023@gmail.com>
2021-04-18 13:32:16 -0300

    Substituindo exibicao dos parametros do Pageable
    Quando o Springfox ler a listagem de cozinha e encontra um Pageable do
    Spring, ele mapeia varios parametros que nao sao os parametros que nosso
    endpoint recebe, criamos uma classe para fins de documentacao para
    representar os parametros que o endpoint recebe e fizemos a substituicao
    do Pageable do spring para o nosso modificado.

--- src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
+++ src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
@@ -6,12 +6,14 @@ import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.algaworks.algafood.api.exceptionhandler.Problem;
+import com.algaworks.algafood.core.openapi.model.PageableModelOpenApi;
 import com.fasterxml.classmate.TypeResolver;
 
 import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
@@ -47,6 +49,7 @@ public class SpringFoxConfig implements WebMvcConfigurer {
 				.globalResponseMessage(RequestMethod.POST, globalPostResponseMessages())
 				.globalResponseMessage(RequestMethod.DELETE, globalDeleteResponseMessages())
 				.additionalModels(typeResolver.resolve(Problem.class)).apiInfo(apiInfo())
+				.directModelSubstitute(Pageable.class, PageableModelOpenApi.class)
 				.tags(new Tag("Cidades", "Gerencia as cidades"), 
 						new Tag("Grupos", "Gerencia os grupos")
 				);
---
+++ src/main/java/com/algaworks/algafood/core/openapi/model/PageableModelOpenApi.java
@@ -0,0 +1,24 @@
+package com.algaworks.algafood.core.openapi.model;
+
+import java.util.List;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+@ApiModel("Pageable")
+@Setter
+@Getter
+public class PageableModelOpenApi {
+
+	@ApiModelProperty(example = "0", value = "Numero da pagina (comeca em 0)")
+	private int page;
+	
+	@ApiModelProperty(example = "10", value = "Quantidade de elementos por pagina")
+	private int size;
+	
+	@ApiModelProperty(example = "nome,asc", value = "Nome da propriedade para ordenacao")
+	private List<String> sort;
+	
+}
