dc6db833262a5ecd74f4d6ddbe9afa74b53867d8
Lucas Henrique <lucas.henrique0023@gmail.com>
2021-04-21 15:24:57 -0300

    Configurando Endpoint FormaPagamento

---
+++ src/main/java/com/algaworks/algafood/api/openapi/controller/FormaPagamentoControllerOpenApi.java
@@ -0,0 +1,50 @@
+package com.algaworks.algafood.api.openapi.controller;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.context.request.ServletWebRequest;
+
+import com.algaworks.algafood.api.exceptionhandler.Problem;
+import com.algaworks.algafood.api.model.FormaPagamentoModel;
+import com.algaworks.algafood.api.model.input.FormaPagamentoInput;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
+@Api(tags = "Formas de Pagamento")
+public interface FormaPagamentoControllerOpenApi {
+
+	@ApiOperation("Lista formas de pagamento")
+	public ResponseEntity<List<FormaPagamentoModel>> listar(ServletWebRequest request);
+	
+	@ApiOperation("Busca uma forma de pagamento por ID")
+	@ApiResponses({
+		@ApiResponse(code = 400, message = "ID da forma de pagamento invalido", response = Problem.class),
+		@ApiResponse(code = 404, message = "Forma de pagamento nao encontrada", response = Problem.class)
+	})
+	public ResponseEntity<FormaPagamentoModel> buscar(Long formaPagamentoId);
+	
+	@ApiOperation("Cadastra uma forma de pagamento")
+	@ApiResponses({
+		@ApiResponse(code = 201, message = "Forma de Pagamento cadastrada")
+	})
+	public FormaPagamentoModel salvar(FormaPagamentoInput formaPagamentoInput);
+	
+	@ApiOperation("Atualiza uma forma de pagamento por ID")
+	@ApiResponses({
+		@ApiResponse(code = 200, message = "Forma de Pagamento atualizada", response = Problem.class),
+		@ApiResponse(code = 404, message = "Forma de Pagamento nao encontrada", response = Problem.class)
+	})
+	public FormaPagamentoModel atualizar(Long formaPagamentoId, FormaPagamentoInput formaPagamentoInput);
+	
+	@ApiOperation("Exclui uma Forma de Pagamento por ID")
+	@ApiResponses({
+		@ApiResponse(code = 204, message = "Forma de Pagamento excluida"),
+		@ApiResponse(code = 404, message = "Forma de Pagamento nao encontrada", response = Problem.class)
+	})
+	public void remover(Long formaPagamentoId);
+	
+}
