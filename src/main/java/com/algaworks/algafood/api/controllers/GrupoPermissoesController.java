30642b5af7f478dd274e197511291f452ead3bf4
Lucas H S Silva <lucas.souza.e.silva@accenture.com>
2021-04-23 14:25:26 -0300

    Implementando documentacao de assosiacao de permissoes com grupos

--- src/main/java/com/algaworks/algafood/api/controllers/GrupoPermissoesController.java
+++ src/main/java/com/algaworks/algafood/api/controllers/GrupoPermissoesController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,12 +15,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.algafood.api.assembler.PermissaoModelAssembler;
 import com.algaworks.algafood.api.model.PermissaoModel;
+import com.algaworks.algafood.api.openapi.controller.GrupoPermissaoControllerOpenApi;
 import com.algaworks.algafood.domain.model.Grupo;
 import com.algaworks.algafood.domain.service.CadastroGrupoService;
 
 @RestController
-@RequestMapping("/grupos/{grupoId}/permissoes")
-public class GrupoPermissoesController {
+@RequestMapping(path = "/grupos/{grupoId}/permissoes",
+        produces = MediaType.APPLICATION_JSON_VALUE)
+public class GrupoPermissoesController implements GrupoPermissaoControllerOpenApi {
 	
 	@Autowired
 	CadastroGrupoService cadastroGrupo;
@@ -36,7 +39,7 @@ public class GrupoPermissoesController {
 	
 	@DeleteMapping("/{permissaoId}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
-	public void desassociarFormaPagamento(@PathVariable Long grupoId, @PathVariable Long permissaoId) {
+	public void desassociarGrupo(@PathVariable Long grupoId, @PathVariable Long permissaoId) {
 		
 		cadastroGrupo.desassociarPermissao(grupoId, permissaoId);
 		
@@ -44,7 +47,7 @@ public class GrupoPermissoesController {
 	
 	@PutMapping("/{permissaoId}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
-	public void associarFormaPagamento(@PathVariable Long grupoId, @PathVariable Long permissaoId) {
+	public void associarGrupo(@PathVariable Long grupoId, @PathVariable Long permissaoId) {
 		
 		cadastroGrupo.associarPermissao(grupoId, permissaoId);
 		
